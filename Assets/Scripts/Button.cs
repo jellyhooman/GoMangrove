46036a26cbcc57ba5966d965dae6da0cbca1182c
Indra Yudha <indrayudha.ind@gmail.com>
2019-10-12 01:53:16 +0700

    update animation

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    //public Animator anim;
+    //public Button btn_setting;
+
+    //void Start()
+    //{
+    //    anim = GetComponent<Animator>();
+    //}
+
+    ////void Update()
+    ////{
+    ////    TaskOnClick();
+    ////}
+
+    //public void TaskOnClick()
+    //{
+    //    anim.Play("slide_setting");
+    //}
+    public void ButtonSetting()
+    {
+        GetComponent<Animator>().SetBool("slide_setting", true);
+    }
+}
