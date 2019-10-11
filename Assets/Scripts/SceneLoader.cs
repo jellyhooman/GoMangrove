025f38d962fb079abfe706b300bbd50ed442a0c0
Indra Yudha <indrayudha.ind@gmail.com>
2019-10-11 17:07:17 +0700

    update fungsional loading screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public Slider slider;
+    private float timeRemaining = 0;
+    private float timeMax = 3f;
+
+    public void Update()
+    {
+        slider.value = CalculateTimer();
+        timeRemaining += Time.deltaTime;
+
+        if (timeRemaining > 3)
+        {
+            SceneManager.LoadScene("Main Scene");
+        }
+    }
+
+    float CalculateTimer()
+    {
+        return (timeRemaining / timeMax);
+    }
+}
+
