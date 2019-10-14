b1cc6d7b2cd10c3f3b0d15652e81a56f2214b10e
Indra Yudha <indrayudha.ind@gmail.com>
2019-10-14 23:01:06 +0700

    fixed ui

---
+++ Assets/Scripts/Main Menu/Main_Button.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main_Button : MonoBehaviour
+{
+    public Image changeIconMusic, changeIconSound;
+
+    public Sprite iconMusicOn, iconSoundOn, iconMusicOff, iconSoundOff;
+
+    private bool conMusic = true;
+    private bool conSound = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void btnMusic()
+    {
+        if (conMusic == true)
+        {
+            changeIconMusic.sprite = iconMusicOff;
+            conMusic = false;
+        } else if (conMusic == false)
+        {
+            changeIconMusic.sprite = iconMusicOn;
+            conMusic = true;
+        }
+    }
+
+    public void btnSound()
+    {
+        if (conSound == true)
+        {
+            changeIconSound.sprite = iconSoundOff;
+            conSound = false;
+        }
+        else if (conSound == false)
+        {
+            changeIconSound.sprite = iconSoundOn;
+            conSound = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Resolution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resolution : MonoBehaviour
+{
+    //inisialisasi obejk
+    public Image changeImage;
+    //inisialisasi sprite untuk merubah objek
+    public Sprite res1080, res1080x2016, res720, defaultRes;
+
+    void Start()
+    {
+        if ((Screen.height == 1920 && Screen.width == 1080) || (Screen.height == 1080 && Screen.width == 1920))
+        {
+            Debug.Log("1080");
+            changeImage.sprite = res1080;
+        }
+        else if ((Screen.height == 1080 && Screen.width == 2160) || (Screen.height == 2160 && Screen.width == 1080))
+        {
+            Debug.Log("1080x2016");
+            changeImage.sprite = res1080x2016;
+        }
+        else if ((Screen.height == 1280 && Screen.width == 720) || (Screen.height == 720 && Screen.width == 1280))
+        {
+            Debug.Log("720");
+            changeImage.sprite = res720;
+        }
+        else
+        {
+            changeImage.sprite = defaultRes;
+        }
+    }
+}
